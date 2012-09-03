a5e981326faa40ebaaa786bd7cbbc55c7ab0d1d2
biofrost88@gmail.com <biofrost88@gmail.com@67704722-acae-f55c-0afb-f3762298e4dc>
2012-09-03 08:09:52 +0000

    Salvataggio dell'oggetto Contact nel ConversationFrame

--- ProgettoSSDRMI/src/layout/conversationframe/Conversation_Table.java
+++ ProgettoSSDRMI/src/layout/conversationframe/Conversation_Table.java
@@ -1,15 +1,17 @@
 package layout.conversationframe;
 
+import javax.swing.JTable;
+
 /**
  * Tabella in cui verranno mostrati (formattati) i messaggi
  * scambiati fra gli utenti della conversazione. 
  * 
  * @author Fabio Pierazzi
  */
-public class Conversation_Table {
+public class Conversation_Table extends JTable {
 	
 	/* Costruttore */
-	public Conversation_Table() {
+	public Conversation_Table(Conversation_TableModel conversation_TableModel) {
 		
 	}
 
